fb39e52525dae5d6e91aca11ce39898fe77b05d7
Matias Trovatto <118058206+MatyTrova@users.noreply.github.com>
2023-07-23 16:13:18 -0300

    Add files via upload

--- Introducción.py
+++ Introducción.py
@@ -11,20 +11,20 @@ st.set_page_config(
 st.title("**Prueba Técnica - Dashboard**")
 
 # Imagen
-#st.image("ruta/a/tu/imagen.jpg", caption="Imagen de ejemplo", use_column_width=True)
+st.image("https://github.com/MatyTrova/prueba/blob/main/imgs/logo-came.svg", caption="logo came", use_column_width=True)
 # Línea divisoria
 st.markdown("---")
 # Encabezado
-st.header("Bienvenido Claudio :)")
+st.header("¡Bienvenido, Claudio!")
 
 # Texto descriptivo
-st.write("Bienvenido al **Dashboard** de la prueba técnica, una aplicación interactiva diseñada para visualizar los datos sobre el estado actual de las pymes del país en diciembre de 2019")
+st.write("Te doy la bienvenida a mi **Dashboard** de la prueba técnica, una aplicación interactiva diseñada para visualizar los datos sobre el estado actual de las pymes del país en diciembre de 2019")
 
 # Sección de información
 st.subheader("¿Qué puedes encontrar aquí?")
-st.write("En esta aplicación, podrás descubrir una dos secciones para el análisis y descubrimiento:")
-st.write("+ **Datos**: Explora una visualización completa y actualizada de los datos recopilados.")
-st.write("+ **Visualizaciones**: Explora gráficos interactivos y descubre de manera sencilla como se comportan los datos")
+st.write("En esta aplicación, descubrirás dos secciones:")
+st.write("+ **Datos**: Visualiza tablas de datos completas y actualizadas.")
+st.write("+ **Visualizaciones**: Explora gráficos interactivos para comprender fácilmente el comportamiento de los datos.")
 
 # Línea divisoria
 st.markdown("---")
