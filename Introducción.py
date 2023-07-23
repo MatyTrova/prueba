bc7b9d813c3559b257c36a2e0be3dffff700efed
Matias Trovatto <118058206+MatyTrova@users.noreply.github.com>
2023-07-23 14:03:09 -0300

    Add files via upload

--- Introducción.py
+++ Introducción.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Prueba técnica - Matias Trovatto",
     page_icon="📊",
-    layout="wide",
+    layout="centred",
     initial_sidebar_state="expanded"
     )
 
